fa88c72d098173544f0844a09e6125cb044671db
marcoMartRuiz <marco.mart.ruiz@gmail.com>
2021-02-14 20:51:39 -0800

    add mapAreaData Loader

--- Assets/Scripts/TerrainGenator.cs
+++ Assets/Scripts/TerrainGenator.cs
@@ -4,76 +4,46 @@ using UnityEngine;
 
 public class TerrainGenator : MonoBehaviour
 {
+    int[] MapAreaNArray;
     private void Start()
     {
-        byte mapAreaValue = 0; // 16 areas
-        LoadMapData(mapAreaValue);
+        LoadMapData();
+        CreateTerrainMesh();       
+
     }
 
-    public void LoadMapData(byte mapAreaValue)
+    public void LoadMapData()
     {
+        int mapSize = 256;
+        int mapAreaColumn = 1;
+        int mapAreaRow = 1;
+
         var LoadMapHeightImage = Resources.Load<Texture2D>("palletHeightMap");
+    
+        int[] MapAreaNArray = new int[mapSize * mapSize ];
+        Color[] CurrentHeightMapArea = LoadMapHeightImage.GetPixels(mapAreaColumn * mapSize , mapAreaRow* mapSize, mapSize, mapSize);
 
-
-        Color[] LoadMapHeightImageArrayNum = LoadMapHeightImage.GetPixels(0, 0, 256, 256);
-
-
-
-
-
-
-        // Color[] LoadMapHeightImageArray0 = LoadMapHeightImage.GetPixels(0, 0, 256, 256);
-        // Color[] LoadMapHeightImageArray1 = LoadMapHeightImage.GetPixels(0, 256, 256, 256);
-        // Color[] LoadMapHeightImageArray2 = LoadMapHeightImage.GetPixels(0, 512, 256, 256);
-        // Color[] LoadMapHeightImageArray3 = LoadMapHeightImage.GetPixels(0, 768, 256, 256);
-
-        // Color[] LoadMapHeightImageArray4 = LoadMapHeightImage.GetPixels(256, 0, 256, 256);
-        // Color[] LoadMapHeightImageArray5 = LoadMapHeightImage.GetPixels(256, 256, 256, 256);
-        // Color[] LoadMapHeightImageArray6 = LoadMapHeightImage.GetPixels(256, 512, 256, 256);
-        // Color[] LoadMapHeightImageArray7 = LoadMapHeightImage.GetPixels(256, 768, 256, 256);
-
-        // Color[] LoadMapHeightImageArray8 = LoadMapHeightImage.GetPixels(512, 0, 256, 256);
-        // Color[] LoadMapHeightImageArray9 = LoadMapHeightImage.GetPixels(512, 256, 256, 256);
-        // Color[] LoadMapHeightImageArray10 = LoadMapHeightImage.GetPixels(512, 512, 256, 256);
-        // Color[] LoadMapHeightImageArray11 = LoadMapHeightImage.GetPixels(512, 768, 256, 256);
-
-        // Color[] LoadMapHeightImageArray12 = LoadMapHeightImage.GetPixels(768, 0, 256, 256);
-        // Color[] LoadMapHeightImageArray13 = LoadMapHeightImage.GetPixels(768, 256, 256, 256);
-        // Color[] LoadMapHeightImageArray14 = LoadMapHeightImage.GetPixels(768, 512, 256, 256);
-        // Color[] LoadMapHeightImageArray15 = LoadMapHeightImage.GetPixels(768, 768, 256, 256);
-
-        // float CurrentMapArea = LoadMapHeightImageArray0[0].grayscale;
-
-
+        for (int y = 0; y < mapSize; y++)
+        {
+            for (int x = 0; x < mapSize; x++)
+            {
+                MapAreaNArray[x + (y * mapSize)] = Mathf.FloorToInt(CurrentHeightMapArea[x + (y * mapSize)].grayscale * 100);
+            }
+        }
     }
 
+    public void CreateTerrainMesh()
+    {
+        
+    }
 
 }
 
-public enum MapAreaValue
-{
-    MapArea0, MapArea1, MapArea2, MapArea3,
-    MapArea4, MapArea5, MapArea6, MapArea7,
-    MapArea8, MapArea9, MapArea10, MapArea11,
-    MapArea12, MapArea13, MapArea14, MapArea15,
+// public enum MapAreaValue
+// {
+//     MapArea0 , MapArea1, MapArea2, MapArea3,
+//     MapArea4, MapArea5, MapArea6, MapArea7,
+//     MapArea8, MapArea9, MapArea10, MapArea11,
+//     MapArea12, MapArea13, MapArea14, MapArea15,
 
-}
-
-// int[] MapHeightGreyScaleArea0 = new int[65536]; // 0-255
-//         // int[] MapHeightGreyScaleArea1 = new int[65536]; // 
-//         // int[] MapHeightGreyScaleArea2 = new int[65536];  
-//         // int[] MapHeightGreyScaleArea3 = new int[65536];  
-//         // int[] MapHeightGreyScaleArea4 = new int[65536];  
-//         // int[] MapHeightGreyScaleArea5 = new int[65536];  
-//         // int[] MapHeightGreyScaleArea6 = new int[65536];  
-//         // int[] MapHeightGreyScaleArea7 = new int[65536];  
-
-//         for (int y = 0; y < 256; y++)
-//         {
-//             for (int x = 0; x < 256; x++)
-//             {
-//                 // int MapHeightGreyScaleValue = Mathf.FloorToInt((LoadMapHeightImageArray[x].b + LoadMapHeightImageArray[x].g + LoadMapHeightImageArray[x].r) / 3);
-//                 MapHeightGreyScaleArray[i] = MapHeightGreyScaleValue;
-//                 Debug.Log(x + " , " + y + " , " + MapHeightGreyScaleArray[i]);
-//             }
-//         }
+// }
