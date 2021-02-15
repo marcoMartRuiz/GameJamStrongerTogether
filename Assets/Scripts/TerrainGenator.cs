18376496b8a9abf362abb90252592450742b76ad
marcoMartRuiz <marco.mart.ruiz@gmail.com>
2021-02-15 01:01:32 -0800

    create terrainmesh out of quads. vector count and indices count needs fixing

--- Assets/Scripts/TerrainGenator.cs
+++ Assets/Scripts/TerrainGenator.cs
@@ -1,40 +1,92 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(MeshFilter))]
 public class TerrainGenator : MonoBehaviour
 {
     int[] MapAreaNArray;
+    Mesh terrainMesh;
+    Vector3[] newVertices;
+    int[] newIndices;
+    int mapSize;
     private void Start()
     {
+        terrainMesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = terrainMesh;
         LoadMapData();
-        CreateTerrainMesh();       
+        CreateTerrainMesh();
+        UpdateTerrainMesh();
 
+    }
+
+    private void Update()
+    {
     }
 
     public void LoadMapData()
     {
-        int mapSize = 256;
+        mapSize = 256;
+
         int mapAreaColumn = 1;
         int mapAreaRow = 1;
 
         var LoadMapHeightImage = Resources.Load<Texture2D>("palletHeightMap");
-    
-        int[] MapAreaNArray = new int[mapSize * mapSize ];
-        Color[] CurrentHeightMapArea = LoadMapHeightImage.GetPixels(mapAreaColumn * mapSize , mapAreaRow* mapSize, mapSize, mapSize);
 
-        for (int y = 0; y < mapSize; y++)
+        int[] MapAreaNArray = new int[mapSize * mapSize];
+        Color[] CurrentHeightMapArea = LoadMapHeightImage.GetPixels(mapAreaColumn * mapSize, mapAreaRow * mapSize, mapSize, mapSize);
+
+        for (int i = 0, y = 0; y < mapSize; y++)
         {
             for (int x = 0; x < mapSize; x++)
             {
-                MapAreaNArray[x + (y * mapSize)] = Mathf.FloorToInt(CurrentHeightMapArea[x + (y * mapSize)].grayscale * 100);
+                MapAreaNArray[i] = Mathf.FloorToInt(CurrentHeightMapArea[i].grayscale * 100);
+
             }
         }
     }
 
     public void CreateTerrainMesh()
     {
-        
+        newVertices = new Vector3[(mapSize + 1) * (mapSize + 1)];
+
+        for (int i = 0, z = 0; z < mapSize; z++)
+        {
+            for (int x = 0; x < mapSize; x++)
+            {
+                newVertices[i] = new Vector3(x, 0, z);
+                i++;
+            }
+        }
+
+        int vert = 0;
+        int quad = 0;
+        newIndices = new int[mapSize * mapSize * 4];
+        for (int z = 0; z < mapSize; z++)
+        {
+            for (int x = 0; x < mapSize; x++)
+            {
+                newIndices[quad + 0] = vert + 0;
+                newIndices[quad + 1] = vert + mapSize;
+                newIndices[quad + 2] = vert + mapSize + 1;
+                newIndices[quad + 3] = vert + 1;
+
+                vert++;
+                quad += 4;
+
+            }
+            // vert++;
+        }
+
+    }
+
+    public void UpdateTerrainMesh()
+    {
+
+        terrainMesh.Clear();
+        terrainMesh.vertices = newVertices;
+        terrainMesh.SetIndices(newIndices, MeshTopology.Quads, 0);
+        terrainMesh.RecalculateNormals();
+
     }
 
 }
